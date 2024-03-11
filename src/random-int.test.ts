106f30ec1e19895fac110013d42875114ce77afc
yano3nora <yano3@alhino.jp>
2024-03-12 02:23:10 +0900

    add seed param into random-int

--- src/random-int.test.ts
+++ src/random-int.test.ts
@@ -4,10 +4,68 @@ import { randomInt } from './random-int.ts'
 /**
  * TODO ランダムで不安定なテスト
  */
-Deno.test('randomInt - returns a number between 0 and 10', () => {
-  for (let index = 0; index < 100; index++) {
-    const result = randomInt(0, 10)
-    assertEquals(typeof result, 'number')
-    assertEquals(result >= 0 && result <= 10, true)
-  }
+Deno.test('randomInt', async test => {
+  await test.step('returns a number between 0 and 10', () => {
+    for (let index = 0; index < 100; index++) {
+      const result = randomInt(0, 10)
+      assertEquals(typeof result, 'number')
+      assertEquals(result >= 0 && result <= 10, true)
+    }
+  })
+
+  await test.step('check bias', () => {
+    const results = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+    }
+
+    for (let index = 0; index < 10000; index++) {
+      const output = randomInt(1, 9) as 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+      results[output]++
+    }
+
+    console.log(results)
+  })
+
+
+  await test.step('check bias with seed', () => {
+    const seed = 'this will seed return ...' // 9
+    const results = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+    }
+
+    for (let index = 0; index < 10000; index++) {
+      const output = randomInt(1, 9, seed) as 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+      results[output]++
+    }
+
+    assertEquals(results, {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 10000,
+    })
+  })
 })
--- src/random-int.ts
+++ src/random-int.ts
@@ -1,11 +1,21 @@
+import { char2codes } from './char-2-codes.ts'
+import { LCG } from "./lcg.ts"
+
 /**
  * @example randomInt(0, 10) // 0 ~ 10
  */
-export const randomInt = (min: number, max: number) => {
+export const randomInt = (min: number, max: number, seed?: string) => {
   let maximum = max + 1
 
   min = Math.ceil(min)
   maximum = Math.floor(maximum)
 
-  return Math.floor(Math.random() * (maximum - min) + min)
+  const prng = seed ? LCG(char2codes(seed), true) : undefined
+  const algo = () => (
+    prng
+      ? prng.next().value
+      : Math.random()
+  )
+
+  return Math.floor(algo() * (maximum - min) + min)
 }
