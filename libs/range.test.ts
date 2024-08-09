ae22a7e200afaff7ee11f5ff7b42e04c65de2d26
yano3nora <yano3@alhino.jp>
2024-08-09 11:38:02 +0900

    enhance range

--- libs/range.test.ts
+++ libs/range.test.ts
@@ -1,9 +1,42 @@
 import { assertEquals } from 'https://deno.land/std@0.186.0/testing/asserts.ts'
 import { range } from './range.ts'
 
-Deno.test('range', () => {
-  assertEquals(
-    range(5),
-    [0, 1, 2, 3, 4],
-  )
+Deno.test('range', async test => {
+  await test.step('simple array', () => {
+    assertEquals(
+      range(0),
+      [],
+    )
+    assertEquals(
+      range(1),
+      [0],
+    )
+    assertEquals(
+      range(5),
+      [0, 1, 2, 3, 4],
+    )
+  })
+
+  await test.step('passed min & max', () => {
+    assertEquals(
+      range({ min: 1, max: 5 }),
+      [1, 2, 3, 4, 5],
+    )
+    assertEquals(
+      range({ min: 0, max: 5 }),
+      [0, 1, 2, 3, 4, 5],
+    )
+    assertEquals(
+      range({ min: 0, max: 0 }),
+      [0],
+    )
+    assertEquals(
+      range({ min: 1, max: 0 }),
+      [],
+    )
+    assertEquals(
+      range({ min: -5, max: 0 }),
+      [-5, -4, -3, -2, -1, 0],
+    )
+  })
 })
--- libs/range.ts
+++ libs/range.ts
@@ -1,4 +1,16 @@
+import { isNumber } from './is-number.ts'
+
 /**
- * @example range(5) // [ 0, 1, 2, 3, 4 ]
+ * @example
+ * range(5) // [ 0, 1, 2, 3, 4 ]
+ * range({ min: 1, max: 5 }) // [1, 2, 3, 4, 5]
  */
-export const range = (num: number) => [...Array(num)].map((_, k) => k)
+export const range = (opt: number | { min: number, max: number }) => {
+  if (isNumber(opt)) {
+    return [...Array(opt)].map((_, k) => k)
+  }
+
+  const { min, max } = opt
+
+  return Array.from({ length: (max - min + 1) }, (_, k) => k + min)
+}
