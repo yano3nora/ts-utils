7f4153589e54c097f97d5ee0f95fbc60bb9854c2
yano3nora <yano3@alhino.jp>
2025-12-03 16:16:59 +0900

    add example comment into objects2object

--- libs/objects-2-object.ts
+++ libs/objects-2-object.ts
@@ -4,6 +4,16 @@
  *     [ { id: 1, name: 'john' }, { id: 2, name: 'jane' } ],
  *     'id',
  *   ) // => { 1: { id: 1, name: 'john' }, 2: { id: 2, name: 'jane' } }
+ *
+ * @example
+ * const _USERS = [
+ *   { key: 'john', age: 35 },
+ *   { key: 'jane', age: 28 },
+ * ] as const
+ *
+ * export type UserKey = typeof _USERS[number]['key']
+ * export type User = typeof _USERS[number]
+ * export const USERS: Record<UserKey, User> = objects2object([..._USERS], 'key')
  */
 export const objects2object = <T extends Record<string, unknown>>(
   objects: T[],
